c8dfe56e2ab01b702edf28a9862565524fef2113
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2021-06-26 15:33:52 +0200

    refactor(mediainfo): proper rpc barebone implementation

--- services/mediainfo/src/amqp/index.ts
+++ services/mediainfo/src/amqp/index.ts
@@ -1,50 +1,45 @@
 import os from "os";
-import { Channel, connect, Message } from "amqplib";
-
+import { connect } from "amqplib";
 import { mediaingest } from "../proto/mediainfo";
-import { ConsumeMessage } from "amqplib/properties";
-
-export type MessageConsumer = (message: ConsumeMessage) => Promise<Uint8Array>;
-
-const replyConsumer: MessageConsumer = async () => {
-  return mediaingest.MediaInfo.encode({
-    format: {
-      name: "pong",
-    },
-  }).finish();
-};
-
-export function createConsumer(channel: Channel, consumer: MessageConsumer) {
-  return async function consumerWrapper(message: Message | null): Promise<void> {
-    if (message === null) {
-      return;
-    }
-
-    try {
-      const reply = await consumer(message);
-      channel.publish("", message.properties.replyTo, Buffer.from(reply), {
-        correlationId: message.properties.correlationId,
-      });
-      channel.ack(message);
-    } catch (error) {
-      console.error({ fields: message.fields, consumerName: consumer.name, error }, "Failed to consume message");
-      channel.reject(message, false);
-    }
-  };
-}
 
 export async function setupAmqp(dsn: string): Promise<void> {
   const connection = await connect(dsn);
   const channel = await connection.createChannel();
-  await channel.prefetch(1, false);
+  await channel.prefetch(1);
 
   const mediaInfoService = new mediaingest.MediaInfoService(() => {
     // noop
   });
-  const queue = await channel.assertQueue(mediaInfoService.getMediaInfo.name);
-  // await channel.bindQueue(queue.queue, "", mediaInfoService.getMediaInfo.name);
+  const queue = await channel.assertQueue(
+    `${mediaingest.MediaInfoService.name}.${mediaInfoService.getMediaInfo.name}`,
+    {
+      durable: false,
+    }
+  );
 
-  await channel.consume(queue.queue, createConsumer(channel, replyConsumer), {
-    consumerTag: os.hostname(),
-  });
+  await channel.consume(
+    queue.queue,
+    (message) => {
+      if (!message) {
+        return console.log("empty message");
+      }
+
+      // get request
+      const req = mediaingest.GetMediaInforRequest.decode(Uint8Array.from(message.content));
+
+      console.log(req, "media info request");
+
+      // prepare response
+      const res = mediaingest.MediaInfo.encode({ format: { name: "pong" } }).finish();
+
+      // send response
+      channel.sendToQueue(message.properties.replyTo, Buffer.from(res), {
+        correlationId: message.properties.correlationId,
+      });
+      channel.ack(message);
+    },
+    {
+      consumerTag: os.hostname(),
+    }
+  );
 }
--- services/mediainfo/src/ping.ts
+++ services/mediainfo/src/ping.ts
@@ -6,38 +6,47 @@ import { connect } from "amqplib";
   const connection = await connect("amqp://localhost");
   try {
     const channel = await connection.createChannel();
+    const replyQueue = await channel.assertQueue("", {
+      exclusive: true,
+    });
 
-    const rpc: RPCImpl = async (method, requestData, callback) => {
+    const rpcCall: RPCImpl = async (method, requestData, callback) => {
       const correlationId = Math.random().toString(36).substring(2);
-      const replyQueue = await channel.assertQueue("", {
-        exclusive: true,
-      });
-      await channel.consume(replyQueue.queue, (message) => {
-        if (!message) {
-          return console.error("empty message");
+
+      await channel.consume(
+        replyQueue.queue,
+        (message) => {
+          if (!message) {
+            return console.log("empty message");
+          }
+
+          if (message.properties.correlationId !== correlationId) {
+            return console.log("other correlation id", message.properties.correlationId);
+          }
+
+          console.log("rpc call response received");
+          callback(null, Uint8Array.from(message.content));
+        },
+        {
+          noAck: true,
         }
+      );
 
-        if (message.properties.correlationId !== correlationId) {
-          return console.error("other correlation id", message.properties.correlationId);
-        }
-
-        console.debug(message.content.toString());
-        callback(null, Uint8Array.from(message.content));
-        channel.ack(message);
-      });
-
-      channel.publish("", method.name, Buffer.from(requestData), {
+      channel.sendToQueue(`${mediaingest.MediaInfoService.name}.${method.name}`, Buffer.from(requestData), {
         replyTo: replyQueue.queue,
         correlationId,
       });
+
+      console.log("rpc call sent");
     };
 
-    const service = new mediaingest.MediaInfoService(rpc);
+    const service = new mediaingest.MediaInfoService(rpcCall);
+    console.log("sending rpc call");
     const mediaInfo = await service.getMediaInfo({ source: "ping" });
     console.log(mediaInfo);
   } catch (e) {
     console.error(e);
-  } finally {
-    await connection.close();
+    // } finally {
+    //   await connection.close();
   }
 })();
